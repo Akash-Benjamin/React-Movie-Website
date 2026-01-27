31ab165be0a92740eb0d6dbba00d8bc8d0577788
Akash-Benjamin <benjaminakash9343@gmail.com>
2026-01-27 22:38:18 +0530

    updated with loading AND part of search function

---
+++ src/contexts/MovieContext.jsx
@@ -0,0 +1,1 @@
+import {createContext, UseContext, useState, useEffects} from React;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -25,11 +25,23 @@ function Home() {
   }, [])
 
 
-  const handleSearch = (e) => {
-    e.preventDefault()
-    alert(searchQuery)
-    setSearchQuery("")
-  }
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if(!searchQuery.trim()) return
+    if (loading) return
+
+    setLoading(true)
+    try{
+      const searchResults = await searchMovies(searchQuery)
+      setMovies(searchResults)
+      setError(null)
+    }catch(err){
+      console.log(err)
+      setError("failed to search movies....")
+    }finally{
+      setLoading(false)
+    }
+  };
 
   return (
     <div className="home">
@@ -45,9 +57,9 @@ function Home() {
 
       {error && <div className ="error-message">{error}</div>}
 
-      {loading ? (<div className="loading">Loading</div>) : <div className="movies-grid">
+      {loading ? (<div className="loading">Loading</div>) : (<div className="movies-grid">
         {movies.map((movie) => (<MovieCard movie={movie} key={movie.id} />))}
-      </div>}
+      </div>)}
     </div>
   );
 }
