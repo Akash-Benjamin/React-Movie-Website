9bc9c8c9a57c6822ef529e97ea10504d3e1f39fc
Akash-Benjamin <benjaminakash9343@gmail.com>
2026-01-30 17:21:59 +0530

    updated with loading AND part of search function

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -1,1 +1,7 @@
-import {createContext, UseContext, useState, useEffects} from React;
+import {createContext, useContext, useState, useEffect} from "React";
+
+const MovieContext = createContext();
+
+export const useMovieContext = () => useContext(MovieContext)
+
+export const MovieProvider = ({children}) => {}
