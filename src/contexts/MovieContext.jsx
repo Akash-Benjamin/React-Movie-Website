09a401d9d47f8ae5d11cbc48e533f2c3c3226fd3
Akash-Benjamin <benjaminakash9343@gmail.com>
2026-01-31 17:39:04 +0530

    partially updated favorites function

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -4,4 +4,20 @@ const MovieContext = createContext();
 
 export const useMovieContext = () => useContext(MovieContext)
 
-export const MovieProvider = ({children}) => {}
+export const MovieProvider = ({children}) => {
+    const [favorites, setFavorites] = useState([])
+
+    useEffect(() => {
+        const storedFavs = localStorage.getItem("favorites")
+
+        if (storedFavs) setFavorites(JSON.parse(storedFavs))
+    }, {})
+
+    useEffect(() => {
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+    },[favorites])
+
+    return <MovieContext.Provider>
+        {children}
+    </MovieContext.Provider>
+}
